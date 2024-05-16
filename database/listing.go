bc43bd45a327e2c21d5f5ca615eaaff827ee4d4b
Ethan <cosinlinker@gmail.com>
2024-05-16 16:55:11 +0800

    fix: remove index:idx_list_bsc_height

--- database/listing.go
+++ database/listing.go
@@ -7,6 +7,6 @@ import (
 type Listing struct {
 	Id            int64           `json:"id" gorm:"primaryKey"`
 	Price         decimal.Decimal `json:"price" gorm:"type:decimal(65,0);"`
-	ListBscHeight uint64          `gorm:"NOT NULL;index:idx_list_bsc_height"`
+	ListBscHeight uint64          `gorm:"NOT NULL;"`
 	GroupId       int64           `json:"group_id" gorm:"uniqueIndex:idx_list_group_id;not null;"`
 }
